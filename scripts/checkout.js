da2425ecb1b025c314f9f15526b80a74b51f5006
adamzeidan <azeidan4906@gmail.com>
2024-08-30 22:30:35 -0700

    Add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,7 @@
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkoutHeader.js";
 //import '../data/cart-class.js';
+import '../data/backend-practice.js';
 
 renderCheckoutHeader();
 renderOrderSummary();
