8a5a6886dd742f72390f3a99400e5b89dca3e95a
adamzeidan <azeidan4906@gmail.com>
2024-08-30 20:44:40 -0700

    Create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkoutHeader.js";
+import '../data/cart-oop.js';
 
 renderCheckoutHeader();
 renderOrderSummary();
